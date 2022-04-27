947690d2aa1095873e0fc557b80e8cd223636974
Thivesh <nithurithujanu@gmail.com>
2022-04-27 13:03:34 +0530

    Removed Day,c_type and corrected DDL

--- Phase2-Project Design document/DDL.sql
+++ Phase2-Project Design document/DDL.sql
@@ -16,7 +16,7 @@ drop table if exists cart cascade ;
 drop table if exists customer cascade ;
 
 
-drop table if exists customer_type cascade ;
+-- drop table if exists customer_type cascade ;
 
 
 drop table if exists employee cascade ;
@@ -43,7 +43,7 @@ drop table if exists day_to_day_dishes cascade ;
 drop table if exists day_to_day_items cascade ;
 
 
-drop table if exists day cascade;
+-- drop table if exists day cascade;
 
 
 drop trigger if exists day_dishes_update on order_dishes cascade;
@@ -64,16 +64,16 @@ drop trigger if exists customer_update_on_order on orders cascade;
 drop function if exists update_customer_on_order;
 
 
-drop trigger if exists date_insert_on_order on orders cascade;
+-- drop trigger if exists date_insert_on_order on orders cascade;
 
 
-drop function if exists insert_date_on_order;
+-- drop function if exists insert_date_on_order;
 
 
-drop trigger if exists date_insert_on_offer on orders cascade;
+-- drop trigger if exists date_insert_on_offer on orders cascade;
 
 
-drop function if exists insert_date_on_offer;
+-- drop function if exists insert_date_on_offer;
 
 
 create table item( item_id serial, item_name text, cost int, quan_inv int default 0, unit text, primary key(item_id));
@@ -95,8 +95,22 @@ create table table_status( table_id serial, loc text, status text check (status 
 create table area( area_id serial, loc text, city text, primary key(area_id));
 
 
-create table customer_type( c_type_id serial, c_type text check (c_type in ('Platinum','Gold','Silver','Normal')), min_num_dishes int, max_num_dishes int, primary key(c_type_id));
+-- create table customer_type( c_type_id serial, c_type text check (c_type in ('Platinum','Gold','Silver','Normal')), min_num_dishes int, max_num_dishes int, primary key(c_type_id));
 
+create table customer(
+    c_id serial,
+    name text,
+    username text not null,
+    pswd text not null,
+    ph_no bigint,
+    addr text,
+    num_orders int default 0,
+    num_dish int default 0,
+    -- c_type_id int,
+    constraint customer_prim primary key(c_id),
+    constraint cus_username_unique unique(username)
+    -- foreign key(c_type_id) references customer_type on delete set null
+);
 
 create table employee(
     e_id serial,
@@ -126,6 +140,9 @@ create table cart
      foreign key(dish_id) references dish on delete cascade);
 
 
+
+create table offer( offer_id serial, name text, discount int, primary key(offer_id));
+
 create table offer_valid(
     offer_id int not null,
     dat date,
@@ -133,32 +150,28 @@ create table offer_valid(
     --c_type_id int,
     unique(offer_id,dat,dish_id),
     foreign key(offer_id) references offer on delete cascade,
-    foreign key(dat) references day on delete cascade,
-    foreign key(dish_id) references dish on delete cascade,
+    -- foreign key(dat) references day on delete cascade,
+    foreign key(dish_id) references dish on delete cascade
     --foreign key(c_type_id) references customer_type on delete cascade
 );
 
-
-create table offer( offer_id serial, name text, discount int, primary key(offer_id));
-
-
-create table day( dat date, day text check (day in ('Mon','Tue','Wed','Thu','Fri','Sat','Sun')), constraint day_prim primary key(dat));
+-- create table day( dat date, day text check (day in ('Mon','Tue','Wed','Thu','Fri','Sat','Sun')), constraint day_prim primary key(dat));
 
 
 create table day_to_day_dishes
     ( dat date, dish_id int, dish_count int, constraint day_dish_prim primary key(dat,dish_id),
-     foreign key(dat) references day on delete cascade,
+    --  foreign key(dat) references day on delete cascade,
      foreign key(dish_id) references dish on delete cascade);
 
 
 create table day_to_day_items
     ( dat date, item_id int, used int, bought int, left_inv int, constraint day_item_prim primary key(dat,item_id),
-     foreign key(dat) references day on delete cascade,
+    --  foreign key(dat) references day on delete cascade,
      foreign key(item_id) references item on delete cascade);
 
 
 create table orders
-    ( order_id serial, c_id int, area_id int, table_id int, dat date not null, received_time time, finished_time time, delivered_time time, delivery_person int, status text check (status in ('Preparing','Out for delivery','Delivered','Served')), order_type text check (order_type in ('Online','Dine')), primary key(order_id),
+    ( order_id serial, c_id int not null, area_id int, table_id int, dat date not null, received_time time, finished_time time, delivered_time time, delivery_person int, status text check (status in ('Preparing','Out for delivery','Delivered','Served')), order_type text check (order_type in ('Online','Dine')), primary key(order_id),
      foreign key(c_id) references customer on delete
      set null,
      foreign key(area_id) references area on delete
@@ -166,8 +179,9 @@ create table orders
      foreign key(delivery_person) references employee on delete
      set null,
      foreign key(table_id) references table_status on delete
-     set null,
-     foreign key(dat) references day on delete cascade);
+     set null
+    --  foreign key(dat) references day on delete cascade
+    );
 
 
 create table order_dishes
@@ -220,32 +234,32 @@ insert on orders
 for each row execute procedure update_customer_on_order();
 
 
-create function insert_date_on_order() returns trigger language plpgsql as $$
-    begin
-        insert into day(dat) values(NEW.dat) on conflict on constraint day_prim do nothing;
+-- create function insert_date_on_order() returns trigger language plpgsql as $$
+--     begin
+--         insert into day(dat) values(NEW.dat) on conflict on constraint day_prim do nothing;
 
-        return NEW;
-    end
-    $$;
-
-
-create trigger date_insert_on_order
-before
-insert on orders
-for each row execute procedure insert_date_on_order();
+--         return NEW;
+--     end
+--     $$;
 
 
-create function insert_date_on_offer() returns trigger language plpgsql as $$
-    begin
-        if(NEW.dat is not null) then
-            insert into day(dat) values(NEW.dat) on conflict on constraint day_prim do nothing;
-        end if;
-        return NEW;
-    end
-    $$;
+-- create trigger date_insert_on_order
+-- before
+-- insert on orders
+-- for each row execute procedure insert_date_on_order();
 
 
-create trigger date_insert_on_offer
-before
-insert on offer_valid
-for each row execute procedure insert_date_on_offer();
+-- create function insert_date_on_offer() returns trigger language plpgsql as $$
+--     begin
+--         if(NEW.dat is not null) then
+--             insert into day(dat) values(NEW.dat) on conflict on constraint day_prim do nothing;
+--         end if;
+--         return NEW;
+--     end
+--     $$;
+
+
+-- create trigger date_insert_on_offer
+-- before
+-- insert on offer_valid
+-- for each row execute procedure insert_date_on_offer();
